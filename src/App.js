d746c71a0e1d599d06944e22071d8c3e5ba0b95b
Andres-Andretti <acorredor407@gmail.com>
2022-01-13 22:52:22 -0500

    completed punk list carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
-import './Components/Header.css'
+import CollectionCard from './Components/CollectionCard';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import PunkList from './Components/Punklist';
+import Punklist from './Components/Punklist';
 
 function App() {
+  const [punkListData, setPunkListData] = useState([])
+  
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0xC4B41bAb353EE276570026d80f83f26E8f4E2630&order_direction=asc'
+      )
+      console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
+    }
+
+    return getMyNfts()
+  }, [])
+
   return (
     <div className='app'>
       <Header  /> 
+      <Punklist punkListData={punkListData} />
     </div>
-  )
+  ) 
 }
 
 export default App;
